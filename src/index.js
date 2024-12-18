f6942e00978d41fd132dc9678b66af57dd6974fa
Behindtheboard <jk2624@gmail.com>
2024-12-17 23:35:20 -0800

    Add menu show functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import "./styles.css";
 
-const dropdownContainer = document.querySelector(".dropdown-container");
-const menuTitle = document.querySelector(".menu-title");
-const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdownContainer = document.getElementById("dropdown-container");
+const menuButton = document.getElementById("menu-button");
+const menuCloseButton = document.getElementById("menu-close-button");
+const dropdownMenu = document.getElementById("dropdown-menu");
 
-menuTitle.addEventListener("click", (e) => {
+menuButton.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
     dropdownMenu.classList.toggle("visible");
   }
 });
 
 window.addEventListener("click", (e) => {
-  if (!dropdownContainer.contains(e.target)) {
-    dropdownMenu.classList.remove("visible");
+  if (!dropdownContainer.contains(e.target) || (e.target.contains(menuCloseButton))) {
+    dropdownMenu.classList.remove("visible"); 
   }
 });
