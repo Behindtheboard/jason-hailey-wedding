4c0a903f93687f7f3a60ac1c7d4d6205ce3b30c6
Behindtheboard <jk2624@gmail.com>
2024-12-17 20:37:47 -0800

    Start

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+const dropdownContainer = document.querySelector(".dropdown-container");
+const menuTitle = document.querySelector(".menu-title");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+menuTitle.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) {
+    dropdownMenu.classList.toggle("visible");
+  }
+});
+
+window.addEventListener("click", (e) => {
+  if (!dropdownContainer.contains(e.target)) {
+    dropdownMenu.classList.remove("visible");
+  }
+});
