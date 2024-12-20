214b1d7e112c0124da3ed2252d10dfadb112b09f
Behindtheboard <jk2624@gmail.com>
2024-12-20 09:20:08 -0800

    Create dropdown module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import "./styles.css";
 import countdown from "./modules/countdown";
 import noDoubleTap from "./modules/noDoubleTap";
+import dropdown from "./modules/dropdown";
 
-const dropdownContainer = document.getElementById("dropdown-container");
-const menuButton = document.getElementById("menu-button");
-const menuCloseButton = document.getElementById("menu-close-button");
-const dropdownMenu = document.getElementById("dropdown-menu");
-
-menuButton.addEventListener("click", (e) => {
-  if (e.target === e.currentTarget) {
-    dropdownMenu.classList.toggle("visible");
-  }
-});
-
-window.addEventListener("click", (e) => {
-  if (!dropdownContainer.contains(e.target) || (e.target.contains(menuCloseButton))) {
-    dropdownMenu.classList.remove("visible"); 
-  }
-});
-
-countdown()
-noDoubleTap()
+dropdown();
+countdown();
+noDoubleTap();
---
+++ src/modules/dropdown.js
@@ -0,0 +1,19 @@
+export default function dropdown() {
+    const dropdownContainer = document.getElementById("dropdown-container");
+    const menuButton = document.getElementById("menu-button");
+    const menuCloseButton = document.getElementById("menu-close-button");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+    
+    menuButton.addEventListener("click", (e) => {
+      if (e.target === e.currentTarget) {
+        dropdownMenu.classList.toggle("visible");
+      }
+    });
+    
+    window.addEventListener("click", (e) => {
+      if (!dropdownContainer.contains(e.target) || (e.target.contains(menuCloseButton))) {
+        dropdownMenu.classList.remove("visible"); 
+      }
+    });
+    
+}
