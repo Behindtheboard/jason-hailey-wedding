c6368990e05cfd16e2c2216295221797ba8b9c5a
Behindtheboard <jk2624@gmail.com>
2025-01-06 11:41:39 -0800

    Close dropdown menu when links clicked

--- src/modules/dropdown.js
+++ src/modules/dropdown.js
@@ -3,6 +3,7 @@ export default function dropdown() {
   const menuButton = document.getElementById("menu-button");
   const menuCloseButton = document.getElementById("menu-close-button");
   const dropdownMenu = document.getElementById("dropdown-menu");
+  const menulinks = document.querySelectorAll("li a");
 
   menuButton.addEventListener("click", (e) => {
     if (e.target === e.currentTarget) {
@@ -11,10 +12,11 @@ export default function dropdown() {
   });
 
   window.addEventListener("click", (e) => {
-    if (
-      !dropdownContainer.contains(e.target) ||
-      e.target.contains(menuCloseButton)
-    ) {
+    const clickedInsideDropdown = dropdownContainer.contains(e.target);
+    const clickedMenuCloseButton = e.target === menuCloseButton;
+    const clickedMenuLink = Array.from(menulinks).some(link => link === e.target);
+
+    if (!clickedInsideDropdown || clickedMenuCloseButton || clickedMenuLink) {
       dropdownMenu.classList.remove("visible");
     }
   });
