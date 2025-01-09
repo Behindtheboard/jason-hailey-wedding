7a11d1f0ce3c196df29844704ff473e6dca0cfdc
Behindtheboard <jk2624@gmail.com>
2025-01-09 12:27:16 -0800

    Add version hashing for cache bust

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
     clean: true,
   },
-  devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/index.html", "./src/index.js"],
+    static: "./dist", 
+    watchFiles: ["src/**/*"], 
+    hot: true, // Enable Hot Module Replacement (HMR)
+    port: 8080,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          process.env.NODE_ENV === "development"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -38,4 +42,13 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(), // Cleans the output directory before each build
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css", // Hashed CSS filenames
+    }),
+  ],
 };
