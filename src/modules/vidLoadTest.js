7b7c369412a32bef4d5c13d36de6c65997e4eaee
Behindtheboard <jk2624@gmail.com>
2025-01-09 00:09:32 -0800

    Add video errors

--- src/modules/vidLoadTest.js
+++ src/modules/vidLoadTest.js
@@ -2,6 +2,8 @@ export default function vidLoadTest() {
   const videos = document.querySelectorAll("video");
   let loadedVideos = 0;
 
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
   videos.forEach((video, i) => {
     if (video.readyState >= 4) {
       // Video is already fully buffered
@@ -13,10 +15,12 @@ export default function vidLoadTest() {
         console.log(`can play through: ${i}`);
         loadedVideos++;
         console.log(`loaded videos: ${loadedVideos}`);
+        video.play = true;
       });
       video.addEventListener("error", () => {
         console.error(`Video failed to load: ${video.currentSrc}`);
-        loadedVideos++; // Count failed resources to avoid freezing
+        loadedVideos++;
+        alert('Awesome video failed to load: Refresh page!') // Count failed resources to avoid freezing
       });
     }
   });
