cfc07f3a79a10776ac27e0501acfedc1fd013d28
Behindtheboard <jk2624@gmail.com>
2025-01-09 16:34:10 -0800

    Add vid load script

--- src/modules/vidLoadTest.js
+++ src/modules/vidLoadTest.js
@@ -4,23 +4,28 @@ export default function vidLoadTest() {
 
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+  const video = document.querySelector("video");
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    if (video.readyState < 3) {
+      // If video isn't fully ready, reload it
+      console.log("Video failed to load, attempting reload...");
+      video.load();
+    }
+  });
+
   videos.forEach((video, i) => {
     if (video.readyState >= 4) {
       // Video is already fully buffered
-      console.log(`buffered:${i}`);
+      // console.log(`buffered:${i}`);
       loadedVideos++;
     } else {
       // Wait for the video to be ready
       video.addEventListener("canplaythrough", () => {
-        console.log(`can play through: ${i}`);
+        // console.log(`can play through: ${i}`);
         loadedVideos++;
-        console.log(`loaded videos: ${loadedVideos}`);
+        // console.log(`loaded videos: ${loadedVideos}`);
         video.play = true;
-      });
-      video.addEventListener("error", () => {
-        console.error(`Video failed to load: ${video.currentSrc}`);
-        loadedVideos++;
-        alert('Awesome video failed to load: Refresh page!') // Count failed resources to avoid freezing
       });
     }
   });
